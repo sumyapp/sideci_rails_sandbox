c45db27d9bb0b7675b21d9cce29eccf133a2e7c0
vexus2 <rinrin900@gmail.com>
2016-08-25 17:37:23 +0900

    eslint設定系を複数追加

---
+++ eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+      "max-len" : 0,
+    },
+    "ecmaFeatures": {
+        "jsx": true,
+    }
+};
